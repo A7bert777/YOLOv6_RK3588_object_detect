60de9964f01acd943cde56a0fe6cf83d742e5fb9
A7bert777 <2506245294@qq.com>
2025-09-15 13:35:58 +0800

    first commit

---
+++ build/CMakeFiles/rknn_yolov6_demo.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/firefly/rknn_play/yolov6/src/main.cc" "/home/firefly/rknn_play/yolov6/build/CMakeFiles/rknn_yolov6_demo.dir/src/main.cc.o"
+  "/home/firefly/rknn_play/yolov6/src/postprocess.cc" "/home/firefly/rknn_play/yolov6/build/CMakeFiles/rknn_yolov6_demo.dir/src/postprocess.cc.o"
+  "/home/firefly/rknn_play/yolov6/src/yolov6.cc" "/home/firefly/rknn_play/yolov6/build/CMakeFiles/rknn_yolov6_demo.dir/src/yolov6.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../"
+  "../utils"
+  "../3rdparty/stb_image"
+  "../3rdparty/jpeg_turbo/include"
+  "../3rdparty/librga/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/firefly/rknn_play/yolov6/build/utils.out/CMakeFiles/imageutils.dir/DependInfo.cmake"
+  "/home/firefly/rknn_play/yolov6/build/utils.out/CMakeFiles/fileutils.dir/DependInfo.cmake"
+  "/home/firefly/rknn_play/yolov6/build/utils.out/CMakeFiles/imagedrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rknn_yolov6_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov6_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolov6_demo.dir/src/postprocess.cc.o"
+  "CMakeFiles/rknn_yolov6_demo.dir/src/yolov6.cc.o"
+  "rknn_yolov6_demo"
+  "rknn_yolov6_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolov6_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
